8e8914e424275e31f80a6a6353c79f58ee8ad43e
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2020-12-23 17:12:45 +0200

    Test that hook and unhook don't crash

--- test/test_native.py
+++ test/test_native.py
@@ -3,3 +3,9 @@ import dolphin_memory_engine
 
 def test_is_hooked_default():
     assert not dolphin_memory_engine.is_hooked()
+
+
+def test_hook_unhook():
+    dolphin_memory_engine.hook()
+    dolphin_memory_engine.un_hook()
+    assert not dolphin_memory_engine.is_hooked()
